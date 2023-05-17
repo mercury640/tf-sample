f073ae5c37a31231ab03b872669f7862b7808d21
mercury640 <54633837+mercury640@users.noreply.github.com>
2023-05-17 14:35:31 -0400

    Update main.tf

---
+++ iam/main.tf
@@ -0,0 +1,10 @@
+variable "user_names" {
+  description = "IAM usernames"
+  type        = set(string)
+  default     = ["user1", "user2", "user3s"]
+}
+
+resource "aws_iam_user" "example" {
+  for_each = var.user_names
+  name  = each.value
+}
